854828e08f76afe6032efdb8fc236d7ce23e19ef
Balahalls17 <sophianascimentomiranda2050@gmail.com>
2024-11-07 12:27:04 -0300

    dia07

---
+++ Assets/script/Menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Menu : MonoBehaviour
+{
+    public void Comecar()
+    {
+        SceneManager.LoadScene("fase1");
+    }
+
+
+
+
+
+}
